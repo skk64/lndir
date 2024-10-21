3aa05ad3f14b7562c3f64d6b4cb9210e72b95702
sam.d.killen@gmail.com <sam.d.killen@gmail.com>
2024-10-21 04:16:22 +0000

    initial commit
    FossilOrigin-Name: a4209d48c7c800408d5760b6c1b68ca26f942c1f10ad1f5abdc6960c93fcbf6e

---
+++ src/lndir.c
@@ -0,0 +1,111 @@
+#include <dirent.h>
+#include <errno.h>
+#include <linux/limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void print_help(const char *prog_name) {
+    const char *help_string =
+        "Usage: %s [OPTIONS] <source_directory> <target_directory>\n"
+        "Recursively create hard links for all files in <source_directory> "
+        "within <target_directory>.\n"
+        "Will create duplicate directories in <target_directory>.\n"
+        "\n"
+        "Options:\n"
+        "  -h, --help        Show this help message and exit\n"
+        "\n"
+        "Example:\n"
+        "  %s /path/to/source /path/to/target\n"
+        "  %s relative/source relative/target\n";
+    printf(help_string, prog_name, prog_name, prog_name);
+}
+
+/// Creates the directory dest_dir, and links all files from src_dir to
+/// dest_dir recursively All links are hard links, as a symbolic link of the
+/// directory can be done with just ln -s
+///
+/// Will return failure if the src_dir cannot be opened
+/// Return 0 on success, or -1 on failure, with errno set
+int link_all_files_in_dir(const char *src_dir, const char *dest_dir) {
+    // Ensure the source directory exists
+    struct stat src_dir_st;
+    if (stat(src_dir, &src_dir_st) == -1 || !S_ISDIR(src_dir_st.st_mode)) {
+        fprintf(stderr, "%d: %s\n\tError opening source directory: %s\n", errno,
+                strerror(errno), src_dir);
+        return -1;
+    }
+
+    // Create target directory with source directory permissions
+    if (mkdir(dest_dir, src_dir_st.st_mode) == -1) {
+        if (errno != EEXIST) {
+            fprintf(stderr, "%d: %s\n\tError creating target directory: %s\n",
+                    errno, strerror(errno), src_dir);
+            return -1;
+        }
+    }
+
+    // Directory iterator
+    DIR *dir = opendir(src_dir);
+    if (!dir) {
+        fprintf(stderr, "%d: %s\n\tError opening source directory: %s\n", errno,
+                strerror(errno), src_dir);
+        return -1;
+    }
+
+    struct dirent *entry;
+    struct stat src_stat;
+    char src_path[PATH_MAX];
+    char dest_path[PATH_MAX];
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+            continue;
+
+        snprintf(src_path, sizeof(src_path), "%s/%s", src_dir, entry->d_name);
+        snprintf(dest_path, sizeof(dest_path), "%s/%s", dest_dir,
+                 entry->d_name);
+
+        if (stat(src_path, &src_stat) == -1) {
+            fprintf(stderr, "%d: %s\n\tCould not stat file (skipping): %s\n",
+                    errno, strerror(errno), src_path);
+            continue;
+        }
+
+        if (S_ISDIR(src_stat.st_mode)) {
+            // Recurse into directories
+            link_all_files_in_dir(src_path, dest_path);
+        } else if (S_ISREG(src_stat.st_mode)) {
+            // Create a hard link for regular files
+            if (link(src_path, dest_path) == -1) {
+                fprintf(stderr,
+                        "%d: %s\n\tFailed to create hard link (skipping): %s\n",
+                        errno, strerror(errno), dest_path);
+            }
+        }
+    }
+
+    closedir(dir);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc == 2 &&
+        (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)) {
+        print_help(argv[0]);
+        exit(EXIT_SUCCESS);
+    }
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <source_directory> <target_directory>\n",
+                argv[0]);
+        fprintf(stderr, "Try '%s --help' for more information\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    const char *source_dir = argv[1];
+    const char *target_dir = argv[2];
+    link_all_files_in_dir(source_dir, target_dir);
+    return errno;
+}
---
+++ src/main.zig
@@ -0,0 +1,160 @@
+const std = @import("std");
+const Dir = std.fs.Dir;
+const Allocator = std.mem.Allocator;
+const assert = std.debug.assert;
+
+pub fn help(bin_name: [*:0]const u8) !void {
+    const help_message: []const u8 =
+        \\ lndir
+        \\Usage: {0s} <source_directory> <target_directory>
+        \\Recursively create hard links for all files in <source_directory> within <target_directory>.
+        \\Will create duplicate directories in <target_directory>.
+        \\
+        \\Options:
+        \\-h, --help        Show this help message and exit
+        \\
+        \\Example:
+        \\{0s} /path/to/source /path/to/target
+        \\{0s} relative/source relative/target
+        \\
+    ;
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+    try stdout.print(help_message, .{bin_name});
+    try bw.flush();
+}
+
+const Args = struct {
+    source_path: ?[*:0]const u8 = null,
+    destination_path: ?[*:0]const u8 = null,
+    help: bool = false,
+
+    fn parse(argv: [][*:0]u8) Args {
+        var args = Args{};
+        for (1..argv.len) |i| {
+            const arg = std.mem.span(argv[i]);
+            if (std.mem.eql(u8, arg, "-h") or std.mem.eql(u8, arg, "--help")) {
+                args.help = true;
+            } else {
+                if (args.source_path == null) {
+                    args.source_path = argv[i];
+                } else if (args.destination_path == null) {
+                    args.destination_path = argv[i];
+                }
+            }
+        }
+        return args;
+    }
+
+    fn assert_valid(args: *const Args) !void {
+        assert(args.source_path != null);
+        assert(args.destination_path != null);
+    }
+};
+
+const a = error.Failure{
+    .F,
+};
+
+pub fn main() !void {
+    const argv = std.os.argv;
+    const args = Args.parse(argv);
+
+    if (args.help or args.source_path == null or args.destination_path == null) {
+        try help(argv[0]);
+        std.process.exit(1);
+    }
+
+    ln_all_files_in_dir(std.fs.cwd(), args.source_path.?, args.destination_path.?) catch |err| {
+        std.debug.print("{any}\n\n", .{err});
+        try help(argv[0]);
+        std.process.exit(1);
+    };
+}
+
+fn ln_all_files_in_dir(dir: Dir, source_path: [*:0]const u8, destination_path: [*:0]const u8) !void {
+    const destination_path_span: []const u8 = std.mem.span(destination_path);
+    const source_path_span: []const u8 = std.mem.span(source_path);
+
+    const source = dir.openDirZ(source_path, .{ .iterate = true, .no_follow = true }) catch |err| {
+        std.debug.print("Error opening {s}\n", .{source_path});
+        return err;
+    };
+
+    // Create destination directory if it doesn't already exist
+    dir.makeDirZ(destination_path) catch |err| switch (err) {
+        error.PathAlreadyExists => {},
+        else => {
+            std.debug.print("Could not create destination '{s}'\nExiting\n", .{destination_path});
+            return err;
+        },
+    };
+    const destination = dir.openDirZ(destination_path, .{ .iterate = true, .no_follow = true }) catch |err| {
+        std.debug.print("Error opening {s}\n", .{destination_path});
+        return err;
+    };
+
+    // Need memory for walker and file path buffers
+    // Walker uses bounded amount of memory, defined by maximum path lengths
+    // so FixedBufferAllocator is suitable
+    // Maximum memory requirement determined by getdents64() syscall (max of u16) plus 2 paths (and some extra for allocator)
+    var mem_buffer: [std.posix.PATH_MAX * 2 + std.math.maxInt(u16) + 0x1000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&mem_buffer);
+    const allocator = fba.allocator();
+
+    var file_count: u64 = 0;
+
+    // Ansi control codes
+    const csi: *const [2:0]u8 = &.{ std.ascii.control_code.esc, '[' };
+    const move_cursor_prev_line = csi ++ "F";
+    const erase_line = csi ++ "K";
+    const overwrite_prev_line = move_cursor_prev_line ++ erase_line;
+
+    const stderr_writer = std.io.getStdErr().writer();
+    var bw = std.io.bufferedWriter(stderr_writer);
+    const stderr = bw.writer();
+    // newline to compensate for first cursor_prev_line code
+    try stderr.print("\n", .{});
+
+    var walker = try source.walk(allocator);
+    defer walker.deinit();
+    while (walker.next() catch |e| blk: {
+        std.debug.print("Error accessing files: {any}\n\n", .{e});
+        break :blk null;
+    }) |p| {
+        try bw.flush();
+        switch (p.kind) {
+            .file, .sym_link => {
+                const source_file = try std.fs.path.joinZ(allocator, &.{ source_path_span, p.path });
+                const destination_file = try std.fs.path.joinZ(allocator, &.{ destination_path_span, p.path });
+                if (std.posix.link(source_file, destination_file)) {
+                    file_count += 1;
+                    try stderr.print(overwrite_prev_line ++ "Linking  {d} files\n", .{file_count});
+                } else |err| {
+                    try stderr.print(overwrite_prev_line ++ "Error creating link '{s}': {any}\n\n", .{ destination_file, err });
+                }
+                allocator.free(destination_file);
+                allocator.free(source_file);
+            },
+            .directory => {
+                destination.makeDirZ(p.path) catch |err| switch (err) {
+                    error.PathAlreadyExists => {
+                        // Should always continue in this case
+                        continue;
+                    },
+                    else => {
+                        // This suggests something has gone rather wrong, but can still attempt to link files/other directories
+                        try stderr.print(overwrite_prev_line ++ "Error creating directory\n\n", .{});
+                        continue;
+                    },
+                };
+            },
+            else => {
+                try stderr.print(overwrite_prev_line ++ "skipping non-file: {s}, {any}\n\n", .{ p.path, p.kind });
+            },
+        }
+    }
+    try stderr.print(overwrite_prev_line ++ "Linked  {d} files\n", .{file_count});
+    try bw.flush();
+}
