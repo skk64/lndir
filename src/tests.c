e44390bbbf4156bef5cd7d64ef9532809d479d07
sam <>
2026-02-06 05:32:53 +0000

    Seconds string list test
    FossilOrigin-Name: 7736812004194519f6e2bcdb16e8a1a2d95e00e5561fc623d1bdade21c3611dc

--- src/tests.c
+++ src/tests.c
@@ -2,9 +2,62 @@
 #define TEST_RUNNER
 // #define DEBUG
 #include "lndir_uring.c"
+#include "string_list.h"
+
+
+#define expectEqualStrings(a, b) { int i = strcmp(a, b); if (i != 0) { printf("MISMATCH: %s  /  %s\n", a, b); return 1; } }
+
+#define strings_len 16
+
+int test_string_list_2() {
+
+    char* strings[strings_len] = { 
+        "various/tests/foo/z/c",
+        "various/tests/foo/z/x/asdf",
+        "various/tests/foo/b",
+        "various/tests/foo/a",
+        "various/tests/basic_in/b",
+        "various/tests/basic_in/foo",
+        "various/tests/basic_in/bar",
+        "various/tests/basic_out/a",
+        "various/tests/basic_out/b",
+        "various/tests/basic_out/foo",
+        "various/tests/basic_out/bar",
+        "various/tests/foo_out/z/x/asdf",
+        "various/tests/foo_out/z/c",
+        "various/tests/foo_out/b",
+        "various/tests/foo_out/a",
+        "various/README.md",
+    };
+
+    StringList list = {};
+    for (int i = 0; i < strings_len; i++) StringList_add_nullterm(&list, strings[i]);
+
+    printf("checking\n");
+
+    StringListIter iter = StringList_iterate(&list);
+    for (int i = 0; i < strings_len; i++) {
+
+// #define expectEqualStrings(a, b) { int i = strcmp(a, b); if (i != 0) { printf("MISMATCH: %s  /  %s\n", a, b); return 1; } }
+        char* a = StringListIter_next(&iter); 
+        char* b = strings[i];
+        int i = strcmp(a, b);
+        if (i != 0) {
+            printf("MISMATCH: %s  ||  %s\n", a, b);
+            return 1;
+        } else {
+            printf("%s\n", a);
+        }
+    }
+    return 0;
+
+}
+
+
+
 
 int test_string_list() {
-    StringList list = StringList_new();
+    StringList list = {};
     StringListIter iter = StringList_iterate(&list);
 
     for (int i = 0; i < 40; i++) {
@@ -27,8 +80,7 @@ int test_string_list() {
 
 
 int test_linking_file_list() {
-
-    StringList list = StringList_new();
+    StringList list = {};
     StringListIter iter = StringList_iterate(&list);
 
     for (int i = 0; i < 1; i++) {
@@ -42,6 +94,7 @@ int test_linking_file_list() {
 }
 
 int main() {
-    test_string_list();
+    // test_string_list();
+    test_string_list_2();
     // test_linking_file_list();
 }
